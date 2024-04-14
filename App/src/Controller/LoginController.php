7bb4ea2881f0839e5eaaa0d03a5cfbed949b1f63
Saiieq <domisku123@gmail.com>
2024-04-14 19:06:12 +0200

    Added some static pages and plain styling from css

--- App/src/Controller/LoginController.php
+++ App/src/Controller/LoginController.php
@@ -9,10 +9,18 @@ use Symfony\Component\Routing\Attribute\Route;
 class LoginController extends AbstractController
 {
     #[Route('/login', name: 'app_login')]
-    public function index(): Response
+    public function login(): Response
     {
         return $this->render('login/index.html.twig', [
             'controller_name' => 'LoginController',
+        ]);
+    }
+
+    #[Route('/register', name: 'app_register')]
+    public function register(): Response
+    {
+        return $this->render('register/index.html.twig', [
+            'controller_name' => 'RegisterController',
         ]);
     }
 }
